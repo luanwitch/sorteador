754ff6d151ca507a9d4727bcb0eda4a3327cafaf
Giovani Casagrande de Castilhos <giovani.castilhos@selbetti.local>
2025-02-11 16:15:04 -0300

    COmmit projeto Sorteador

---
+++ gruntjs3/gruntfile.js
@@ -0,0 +1,95 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: { 
+            development: { 
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
+                }
+            }, 
+            production: { 
+                options: { 
+                    compress: true 
+                },
+                files: { 
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        },
+
+        replace: { 
+            dev: { 
+                options: { 
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+
+            dist: { 
+                options: { 
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            },
+        },
+
+        htmlmin: { 
+            dist: { 
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                },
+                files: { 
+                    'prebuild/index.html': 'src/index.html'
+                }
+            }
+        },
+
+        watch: { 
+            less: { 
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
+            },
+            replace: { 
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
+            }
+        }
+    });
+
+    // Carrega os plugins necessários
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    // Define as tarefas
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['clean', 'less:production']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist','replace:dist']);
+};
